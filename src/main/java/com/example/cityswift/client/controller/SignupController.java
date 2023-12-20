9f34e8801b36197a4005a2b4220d8bf465c6b967
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2023-12-20 21:30:14 +0100

    adding password requirements

--- src/main/java/com/example/cityswift/client/controller/SignupController.java
+++ src/main/java/com/example/cityswift/client/controller/SignupController.java
@@ -13,6 +13,8 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupController {
 
@@ -40,12 +42,15 @@ public class SignupController {
     private Label reactionMessageLabel;
     @FXML
     private TextField cityTextField;
+    @FXML
+    private Label incorrectDataMessageLabel;
 
 
     public void signupButtonOn(ActionEvent event) {
 
         if (!mailTextField.getText().isBlank()
                 && !passwordField.getText().isBlank()
+                && checkPasswordRequirements(passwordField.getText())
                 && !surnameTextField.getText().isBlank()
                 && !nameTextField.getText().isBlank()
                 && !mobileTextField.getText().isBlank()
@@ -54,12 +59,17 @@ public class SignupController {
                 && !homeNrTextField.getText().isBlank()
                 && !doorKeyTextField.getText().isBlank()
                 && !cityTextField.getText().isBlank()
-                && birthdayDatePicker.getValue() !=null ) {
+                && birthdayDatePicker.getValue() != null) {
+
+            checkPasswordRequirements(passwordField.getText());
+            while (passwordField.getText().isBlank()) {
+                checkPasswordRequirements(passwordField.getText());
+            }
 
             ServerResponse serverResponse = sendSignupRequest(nameTextField.getText(), surnameTextField.getText(),
                     mailTextField.getText(), passwordField.getText(), mobileTextField.getText(),
-                    cityTextField.getText(), streetNameTextField.getText(), postalCodeTextField.getText(),homeNrTextField.getText(),
-                    doorKeyTextField.getText(),birthdayDatePicker.getValue());
+                    cityTextField.getText(), streetNameTextField.getText(), postalCodeTextField.getText(),
+                    homeNrTextField.getText(), doorKeyTextField.getText(), birthdayDatePicker.getValue());
 
             handleSignupResponse(event, serverResponse);
 
@@ -69,13 +79,13 @@ public class SignupController {
     }
 
     private ServerResponse sendSignupRequest(String name, String surname, String mail, String password,
-                                             String mobile, String city, String street,String postalCode,String homeNr,
-                                             String doorKeyNr, LocalDate birthday) {
+                                             String mobile, String city, String street, String postalCode,
+                                             String homeNr, String doorKeyNr, LocalDate birthday) {
         NetworkClient networkClient = new NetworkClient();
 
         try {
-            CreateUserData newUserData = new CreateUserData(name, surname, mail, password, mobile,city,street,postalCode,
-                    homeNr,doorKeyNr, birthday);
+            CreateUserData newUserData = new CreateUserData(name, surname, mail, password, mobile, city, street,
+                    postalCode, homeNr, doorKeyNr, birthday);
             ClientRequest request = new ClientRequest("signup", newUserData);
 
             return networkClient.sendRequest(request);
@@ -96,4 +106,22 @@ public class SignupController {
             }
         }
     }
+
+    public boolean checkPasswordRequirements(String password) {
+
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$");
+        Matcher matcher = pattern.matcher(password);
+
+        if (password.length() < 8) {
+            incorrectDataMessageLabel.setText("Twoje hasło zawiera mniej niz 8 znakow");
+            passwordField.clear();
+            return false;
+        } else if (!matcher.matches()) {
+            incorrectDataMessageLabel.setText("Twoje hasło nie zawiera min jednej małej litery," +
+                                                "\ndużej litery lub cyfry");
+            passwordField.clear();
+            return  false;
+        }else return true;
+    }
+
 }
